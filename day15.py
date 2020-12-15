990e88d3e98ea27b5b79065b5a68c8980525e575
Marc S. Schneider <marc.s.schneider@mailbox.org>
2020-12-15 08:46:05 +0100

    added variant with sparse matrix as pos array

--- day15.py
+++ day15.py
@@ -7,6 +7,7 @@ Created on Tue Dec 15 06:00:31 2020
 """
 
 import numpy as np
+from scipy import sparse
 from sys import getsizeof
 
 inp = [6,3,15,13,1,0]
@@ -19,11 +20,13 @@ lastoc[:len(inp)] = range(len(inp))
 seclastoc = -1*np.ones(M, dtype=int)
 curpos = len(inp)
 curnr = nrs[curpos-1]
-#nrseq = []
 cidx = curpos+1
-pos = -1*np.ones(N, dtype=int)  # this is crucial: store positions in a list; otherwise we spend an eternity searching for them!
+# this is crucial: store positions in a list; otherwise we spend an eternity searching for them!
+#pos = sparse.lil_matrix((1,N), dtype=int)  # sparse array saves a lot of memory, but normal np array is faster!
+pos = -1*np.ones(N, dtype=int)
 
 for k,i in enumerate(inp):  # write position of the initial elements
+#    pos[0,i] = k
     pos[i] = k
 
 for i in range(len(inp), N):   
@@ -35,16 +38,17 @@ for i in range(len(inp), N):
     if ((i+1) % 500000 == 0):
         print(f"Turn {i+1}: curnr = {curnr}, at array pos {curpos} out of {M}")
         
-    #nrseq.append(curnr)
-    residx2 = pos[curnr]
+#    residx = pos[0,curnr]
+    residx = pos[curnr]
     
-    if (residx2 != -1): # res is already present
-        cidx = residx2
+    if (residx != -1): # res is already present
+        cidx = residx
         seclastoc[cidx] = lastoc[cidx]
         lastoc[cidx] = i
     else:               # res is not yet present
         nrs[curpos] = curnr
-        pos[curnr] = curpos  # save position of new element!
+#        pos[0,curnr] = curpos  # save position of new element!
+        pos[curnr] = curpos
         lastoc[curpos] = i
         curpos += 1
         cidx = curpos
