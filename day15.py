4a6179c583bc6ab412c76b9f72b7399bdbbae406
mss2304 <75536648+mss2304@users.noreply.github.com>
2020-12-15 11:28:43 +0100

    day 15: added dictionary or list instead of array for pos

--- day15.py
+++ day15.py
@@ -12,7 +12,7 @@ from sys import getsizeof
 
 inp = [6,3,15,13,1,0]
 N = 30000000
-M = N//8
+M = N//4
 nrs = -1*np.ones(M, dtype=int)
 nrs[:len(inp)] = inp
 lastoc = -1*np.ones(M, dtype=int)
@@ -21,14 +21,17 @@ seclastoc = -1*np.ones(M, dtype=int)
 curpos = len(inp)
 curnr = nrs[curpos-1]
 cidx = curpos+1
-# this is crucial: store positions in a list; otherwise we spend an eternity searching for them!
-#pos = sparse.lil_matrix((1,N), dtype=int)  # sparse array saves a lot of memory, but normal np array is faster!
-pos = -1*np.ones(N, dtype=int)
+# this is crucial: store positions in an array (or dictionary); otherwise we spend an eternity searching for them!
+#pos = sparse.lil_matrix((1,N), dtype=int)  # sparse array saves a lot of memory, but approx 2x the time of full array
+#pos = -1*np.ones(N, dtype=int) # normal numpy array - consumes a lot of memory, but is fast
+pos = {} # dictionary - less memory than array and equally fast
+#pos, pos2 = [], [] # using two lists: takes forever
 
 for k,i in enumerate(inp):  # write position of the initial elements
-#    pos[0,i] = k
-    pos[i] = k
-
+#    pos[0,i] = k # for sparse array
+    pos[i] = k   # for np.array or dict
+#    pos.append(i); pos2.append(k) # for list
+    
 for i in range(len(inp), N):   
     if (seclastoc[cidx] == -1): # spoken the first time
         curnr = 0
@@ -38,17 +41,23 @@ for i in range(len(inp), N):
     if ((i+1) % 500000 == 0):
         print(f"Turn {i+1}: curnr = {curnr}, at array pos {curpos} out of {M}")
         
-#    residx = pos[0,curnr]
-    residx = pos[curnr]
+#    residx = pos[0,curnr] # for sparse array
+#    residx = pos[curnr]   # sufficient for np.array
+    try:    # try catch required for dict and list
+        residx = pos[curnr] # for dict
+        #residx = pos2[pos.index(curnr)] # for list
+    except (KeyError, ValueError):
+        residx = -1
     
     if (residx != -1): # res is already present
         cidx = residx
         seclastoc[cidx] = lastoc[cidx]
         lastoc[cidx] = i
     else:               # res is not yet present
-        nrs[curpos] = curnr
-#        pos[0,curnr] = curpos  # save position of new element!
-        pos[curnr] = curpos
+        nrs[curpos] = curnr 
+#        pos[0,curnr] = curpos  # for sparse array
+        pos[curnr] = curpos    # for np.array or dict 
+ #       pos.append(curnr); pos2.append(curpos) # for list
         lastoc[curpos] = i
         curpos += 1
         cidx = curpos
